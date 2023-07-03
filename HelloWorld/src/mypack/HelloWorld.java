668dff67899dd0b4b6bf96f169cb474364808d32
Stephen DeRosa <stephen_derosa@outlook.com>
2023-07-03 16:26:56 -0400

    Add files via upload
    Hello World files for Java

---
+++ HelloWorld/src/mypack/HelloWorld.java
@@ -0,0 +1,11 @@
+package mypack;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+	public static void main(String[] args)
+	{
+		String name = JOptionPane.showInputDialog("What is your name?");
+		JOptionPane.showMessageDialog(null, "Hello, "+name+"!");
+	}
+}
